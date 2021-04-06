d8a60f7bfff0bf9332d49313b6d29f8ab88add88
Nikita <main.acr0matic@gmail.com>
2021-04-06 11:31:46 +0300

    feat: подключение слайдера

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+/* global Swiper */
+
+// eslint-disable-next-line no-unused-vars
+const sectionSlider = new Swiper('#section-slider', {
+  speed: 1000,
+  mousewheel: true,
+  simulateTouch: false,
+});
