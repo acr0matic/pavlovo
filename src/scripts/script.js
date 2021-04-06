038f264acbcd9e9ed283dfce13c870af2948d237
Nikita <main.acr0matic@gmail.com>
2021-04-06 12:51:14 +0300

    feat: верстка "Оставить мнение"

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,8 +1,25 @@
 /* global Swiper */
 
-// eslint-disable-next-line no-unused-vars
+const headerMenu = document.querySelectorAll('#header .nav__item, .header__action');
 const sectionSlider = new Swiper('#section-slider', {
-  speed: 1000,
+  speed: 500,
   mousewheel: true,
   simulateTouch: false,
 });
+
+sectionSlider.on('slideChange', () => {
+  const index = sectionSlider.realIndex - 1;
+
+  headerMenu.forEach((item) => item.classList.remove('nav__item--current'));
+  if (headerMenu[index]) headerMenu[index].classList.add('nav__item--current');
+});
+
+function SetAnchors(menu) {
+  menu.forEach((item, index) => {
+    item.addEventListener('click', () => {
+      sectionSlider.slideTo(index + 1);
+    });
+  });
+}
+
+SetAnchors(headerMenu);
